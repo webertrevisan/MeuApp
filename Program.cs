07fb6e97ddc4c791ec2af97fae986fd29ab7bc01
Weber Trevisan <weber.trevisan@gmail.com>
2025-02-28 22:54:42 -0300

    Numeros Reais Float, double e Decimal

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MeuApp
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario = 2.500f;
+            double salarioSemestral = 22.000;
+            decimal salarioAnual = 25.000m;
+
+        }
+    }
+}
