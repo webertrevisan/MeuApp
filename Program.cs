6bb1ae399c89a670061fb4ea24f1bacecf40b445
Weber Trevisan <weber.trevisan@gmail.com>
2025-02-28 23:49:11 -0300

    Numeros Reais Float, Double, Decimal

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //Numeros Reais
             float salario = 2.500f;
             double salarioSemestral = 22.000;
             decimal salarioAnual = 25.000m;
